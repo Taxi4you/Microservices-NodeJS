f236088d5ba840c1672f61a9e74ff40564974966
Taxi4you <41004207+Taxi4you@users.noreply.github.com>
2023-08-03 10:44:08 +0300

    Proposes better usages of app object and server object

--- apps/payment/src/main.ts
+++ apps/payment/src/main.ts
@@ -17,3 +17,10 @@ const port = Number(process.env.PAYMENT_SERVICE_PORT);
 app.listen(port, host, () => {
     console.log(`Service is ready and running on path: http://${host}:${port}`);
 });
+
+// consider export app individually so you can use it later for tests using Jest with Supertest or something.
+// For example, create a file /bin/app.ts and export just the app.
+
+// also, get the returned server object from the app.listen method.
+// This can be used to handle gracefull shutdown when SIGTERM events are received.
+// This is extremley crucial. 
